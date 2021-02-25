197462faf01193d96a6636f9f5b9ad1110a83f07
LaurenVreeken <lvreeken@uw.edu>
2021-02-24 18:06:50 -0800

    added no cors error fix

--- map-api/api.py
+++ map-api/api.py
@@ -66,9 +66,11 @@ def login():
 @app.route('/api/v1.0/subjects', methods=['GET'])
 @login_required
 def subjects():
-  subjects = subject_storage.get_subjects()
-
-  return jsonify(wrap_with_status(subjects))
+    subjects = subject_storage.get_subjects()
+    response = jsonify(wrap_with_status(subjects))
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    #return jsonify(wrap_with_status(subjects))
+    return response
 
 
 #https://<>/api/v1.0/subject/3df5a521-f493-40df-a26e-ee70a0019300/tracks?since=2019-05-06
@@ -82,16 +84,22 @@ def subject():
 @app.route('/api/v1.0/subject/<string:subject_id>/tracks', methods=['GET'])
 @login_required
 def subject_tracks(subject_id):
-  subject = subject_storage.get_subject(subject_id)
-  return jsonify(wrap_with_status(subject))
+    subject = subject_storage.get_subject(subject_id)
+    response = jsonify(wrap_with_status(subject))
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    #return jsonify(wrap_with_status(subject))
+    return response
 
-
+#https://<>/static/ranger-blue.svg
 @app.route('/static/<string:image_name>', methods=['GET'])
 def static_image(image_name):
     fh = tempfile.TemporaryFile('w+b')
     mimetype = subject_storage.get_static_image(fh, image_name)
     fh.seek(0)
-    return send_file(fh, mimetype=mimetype)
+    response = send_file(fh, mimetype=mimetype)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    #return send_file(fh, mimetype=mimetype)
+    return responses
 
 @app.route('/media/<string:image_name>', methods=['GET'])
 def static_media(image_name):
